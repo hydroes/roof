a6cdeb10fb6854abadfed3defc0dad8b6d43a523
Brian Gouws <brian.gouws@bauermedia.co.uk>
2018-01-08 17:42:40 +0000

    Update: Basic redux-saga functionality with location example (refs: roof)

--- src/containers/Posts.js
+++ src/containers/Posts.js
@@ -5,11 +5,18 @@ const Posts = ({getPostsClick}) => {
   return <button onClick={() => getPostsClick()}>Get posts</button>
 }
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch, props) => {
   return {
     getPostsClick: () => {
       dispatch({type: 'POSTS_REQUEST'})
-      console.log('getPostsClick')
+      console.log('getPostsClick props', props)
+
+      const location = {
+        pathname: '/about',
+        state: { fromDashboard: true }
+      }
+
+      props.history.push(location)
     }
   }
 }
